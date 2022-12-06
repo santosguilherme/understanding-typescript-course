71449f91a85917f2f31abf9858b2c0006a086db3
Guilherme Santos <sntguilherme@gmail.com>
2022-12-06 17:16:38 +0100

    tsconfig configuration

---
+++ config-starting-setup/src/app.ts
@@ -0,0 +1,7 @@
+const userName = 'Guilherme';
+console.log(userName);
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", () => {
+    console.log("clicked!");
+})
