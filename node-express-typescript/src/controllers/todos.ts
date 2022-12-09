1bef295c8e04ad423d1160d49a750b05a7d4f055
Guilherme Santos <sntguilherme@gmail.com>
2022-12-09 10:42:17 +0100

    node and express project

---
+++ node-express-typescript/src/controllers/todos.ts
@@ -0,0 +1,50 @@
+import {RequestHandler} from "express";
+
+import {Todo} from '../models/todo';
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+    const text = (req.body as { text: string }).text;
+    const newTodo = new Todo(Date.now().toString(), text);
+
+    TODOS.push(newTodo);
+
+    res.status(201).json({message: 'Created.', todo: newTodo});
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+    res.json({todos: TODOS});
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+    const todoId = req.params.id;
+
+    const updatedText = (req.body as { text: string }).text;
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error('Could not find the todo with ID to updated: ' + todoId);
+    }
+
+    TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+    res.status(201).json({message: 'Updated.', todo: TODOS[todoIndex]});
+};
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+    const todoId = req.params.id;
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error('Could not find the todo with ID to deleted: ' + todoId);
+    }
+
+    const todo = TODOS[todoIndex];
+
+    TODOS.splice(todoIndex, 1);
+
+    res.json({message: "Deleted.", todo});
+};
